ff123c1064093fcd30f237119ae447c228713e4b
Mayurk26 <89087181+Mayurk26@users.noreply.github.com>
2023-05-16 01:26:26 +0530

    Delete QuizApp directory

--- QuizApp/quiz/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('',views.login,name='Quizlogin'),
-    path('home/',views.index,name='Quizindex'),
-    path('web/',views.web,name='Quizweb'),
-    path('js/',views.js,name='Quizjs'),
-]
